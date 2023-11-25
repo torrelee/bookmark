c13a13befa1d05ad1b857052629d66f08504a3fe
torrelee <torrelee.jaehyung@gmail.com>
2023-11-25 15:00:20 +0900

    add static_root

--- bookmark/config/settings.py
+++ bookmark/config/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
